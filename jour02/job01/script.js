88391e90d50f4886a1537ffe16172d42a4309637
cedric-prunier <cedric-prunier@laplateforme.io>
2023-07-04 13:06:33 +0200

    premier

---
+++ jour02/job01/script.js
@@ -0,0 +1,5 @@
+function Citation() {
+        const citationElement = document.getElementById("citation");
+        const citationText = citationElement.textContent;
+        console.log(citationText);
+}
