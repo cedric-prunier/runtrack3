7724e09707352df20ffbed961200b58b28a8acb9
cedric-prunier <cedric-prunier@laplateforme.io>
2023-07-07 12:40:49 +0200

    premier

---
+++ jour03/job01/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+   
+</head>
+<body>
+
+<button id="showButton" onclick="show()">Afficher le texte</button>
+<button id="hideButton" onclick="hide()">Cacher le texte</button>
+<p id="myText" style="display: none;">Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit, ensuite on prie.</p>
+
+</body>
+<script src="script.js"></script>
+</html>
---
+++ jour03/job01/script.js
@@ -0,0 +1,8 @@
+function show() {
+        let myText = document.getElementById("myText");
+        myText.style.display = "block";
+}
+function hide() {
+        let myText = document.getElementById("myText");
+        myText.style.display = "none";
+}
